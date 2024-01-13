04c1406d80ae78c07d1b017cff902c48dd81e785
madinaatakhanova <atahanovamadina02@gmail.com>
2024-01-13 18:27:17 +0500

    first-commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+  
+<>
+<h1 className='now'>Pricing</h1>
+<div className="container">
+
+  <div className='card pro'>
+    <h1>Pro</h1>
+    <p>1% / transaction </p>
+    <p className='p'>virtual terminal</p>
+    <p className='p'>ultimate spaces</p>
+  </div>
+  <div className='card premium'>
+    <h1>Premium</h1>
+    <p>1% + $ 12 / mounths</p>
+    <p className='p'>dicter deverite</p>
+    <p className='p'>vertual terminal</p>
+    <p className='p'>ultimate spaces</p>
+  </div>
+  <div className='card platinum'>
+    <h1>Platinum</h1>
+    <p>1% + $ 29 / mounths</p>
+    <p className='p'>requer building</p>
+    <p className='p'>dicter deverite</p>
+    <p className='p'>vertual terminal</p>
+    <p className='p'>ultimate spaces</p>
+  </div>
+    </div>
+</>
+  );
+}
+
+export default App;
